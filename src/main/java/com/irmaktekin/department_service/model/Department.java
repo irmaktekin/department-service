d4722c78af37aee45435d65393b1bdd951490267
irmaktekin <irmaktekin97@gmail.com>
2024-11-28 00:23:13 +0300

    add Department and Employee models

---
+++ src/main/java/com/irmaktekin/department_service/model/Department.java
@@ -0,0 +1,51 @@
+package com.irmaktekin.department_service.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+    private Long id;
+    private String name;
+    private List<Employee> employees = new ArrayList<>();
+
+    public Department() {
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", employees=" + employees +
+                '}';
+    }
+
+    public Department(String name, Long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
